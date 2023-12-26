f2a70f77d092ecdbb7f6717f35cfb475378a0d61
Shion-0014 <seanmqe@gmail.com>
2023-12-26 18:46:48 +0900

    FIX: bugs

--- __init__.py
+++ __init__.py
@@ -4,21 +4,6 @@ from PIL import Image
 import torch
 import numpy as np
 
-# Tensor to PIL
-def tensor2pil(image_tensor):
-    image_np = image_tensor.cpu().numpy().squeeze()
-    image_np = np.clip(image_np * 255, 0, 255).astype(np.uint8)
-    if image_np.ndim == 2:  # Grayscale image
-        return Image.fromarray(image_np, mode='L')
-    return Image.fromarray(image_np)
-
-# Convert PIL to Tensor
-def pil2tensor(image_pil):
-    image_np = np.array(image_pil).astype(np.float32) / 255.0
-    if image_np.ndim == 2:  # Grayscale image
-        image_np = np.expand_dims(image_np, axis=2)
-    return torch.from_numpy(image_np).unsqueeze(0)
-
 class ImageRemoveBackgroundRembg:
     def __init__(self):
         pass
@@ -32,20 +17,42 @@ class ImageRemoveBackgroundRembg:
         }
 
     RETURN_TYPES = ("IMAGE",)
-    FUNCTION = "remove_video_background"
+    FUNCTION = "remove_background"
     CATEGORY = "image"
 
+    def tensor_to_pil(self, img):
+        if img is not None:
+            i = 255. * img.cpu().numpy().squeeze()
+            img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+        return img
+
+    def add_green_background(self, image):
+        # Create a green background
+        green_background = Image.new("RGB", image.size, (0, 255, 0))
+
+        # Overlay the transparent image on the green background
+        green_background.paste(image, (0, 0), image)
+        return green_background
+
     def remove_background(self, images):
-        processed_images = []
-        for image_tensor in images:
-            pil_image = tensor2pil(image_tensor)
-            removed_bg_image = remove(pil_image)
-            tensor_image = pil2tensor(removed_bg_image)
-            processed_images.append(tensor_image)
-        return tuple(processed_images,)
+        # Create empty tensor list
+        total_images = []
+        for image in images:
+            image = self.tensor_to_pil(image)
+            image = remove(image)
+
+            # Add green background
+            image_with_green_bg = self.add_green_background(image)
+
+            # Convert to tensor
+            total_images.append(torch.from_numpy(np.array(image_with_green_bg).astype(np.float32) / 255.0).unsqueeze(0))
+
+        total_images = torch.cat(total_images, 0)
+        return (total_images,)
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
     "Video Remove Background (rembg)": ImageRemoveBackgroundRembg
 }
+
